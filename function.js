081ce33888a6fda9fe1ce866f070e5a3f0d9214b
shiva <shivamoeini2000@gmail.com>
2022-01-11 20:56:46 +0330

    cmit2-shiva

--- function.js
+++ function.js
@@ -32,9 +32,27 @@
 
 
 
-var myFunc;
-if (num === 0) {
-  myFunc = function(theObject) {
-    theObject.make = 'Toyota';
+// var myFunc;
+// if (num === 0) {
+//   myFunc = function(theObject) {
+//     theObject.make = 'Toyota';
+//   }
+// }
+
+
+
+
+function map(f, a) {
+    let result = []; // Create a new Array
+    let i; // Declare variable
+    for (i = 0; i != a.length; i++)
+      result[i] = f(a[i]);
+    return result;
   }
-}
+  const f = function(x) {
+     return x * x * x;
+  }
+  let numbers = [0, 1, 2, 5, 10];
+  let cube = map(f,numbers);
+  console.log(cube);
+  
