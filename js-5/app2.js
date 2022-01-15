2b61878a0dc587fca3f14132c74000749201cad2
shiva <shivamoeini2000@gmail.com>
2022-01-15 14:23:27 +0330

    cmit9shiva

---
+++ js-5/app2.js
@@ -0,0 +1,29 @@
+console.log("coconuts".slice(4, 7));
+// → nut
+console.log("coconut".indexOf("u"));
+// → 5
+
+
+
+
+// One difference is that a string’s indexOf can search for a string containing more than one character, whereas the corresponding array method looks only for a single element.
+
+console.log("one two three".indexOf("ee"));
+// → 11
+
+
+// The trim method removes whitespace (spaces, newlines, tabs, and similar characters) from the start and end of a string.
+
+console.log("                         okay \n ".trim());
+// → okay
+
+
+console.log(String(6).padStart(3, "0"));
+// → 006
+// You can split a string on every occurrence of another string with split and join it again with join.
+
+
+// A string can be repeated with the repeat method, which creates a new string containing multiple copies of the original string, glued together.
+
+console.log("LA".repeat(3));
+// → LALALA
