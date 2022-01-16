aaabe6185f30dc9130e4fe17ebfff995c1a2823d
shiva <shivamoeini2000@gmail.com>
2022-01-16 14:03:17 +0330

    cmitt14shivaa

---
+++ app3.js
@@ -0,0 +1,9 @@
+var myObject = { };
+
+myObject[true] = "foo";
+myObject[3] = "bar";
+myObject[myObject] = "baz";
+
+myObject["true"];				// "foo"
+myObject["3"];					// "bar"
+myObject["[object Object]"];	// "baz"
