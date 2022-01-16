4206f95e56fffbb73e3d4239948a02755be1700a
shiva <shivamoeini2000@gmail.com>
2022-01-16 14:05:39 +0330

    cmit15shivva

---
+++ app5.js
@@ -0,0 +1,20 @@
+var newObj = Object.assign( {}, myObject );
+
+newObj.a;						// 2
+newObj.b === anotherObject;		// true
+newObj.c === anotherArray;		// true
+newObj.d === anotherFunction;	/
+
+
+
+
+var myObject = {};
+
+Object.defineProperty( myObject, "a", {
+	value: 2,
+	writable: true,
+	configurable: true,
+	enumerable: true
+} );
+
+myObject.a; // 2
