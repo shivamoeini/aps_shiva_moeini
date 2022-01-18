77a21e1d3ff66b7e7ad13e74442801115a1acd24
shiva <shivamoeini2000@gmail.com>
2022-01-18 18:28:19 +0330

    cmit23shiva

---
+++ js-9/app1.js
@@ -0,0 +1,13 @@
+// x = prompt("enter a number1")
+// y = prompt("enter a number2")
+// if (x >= y) {
+//     alert(true);
+// } else {
+//     alert(false);
+// }
+
+// if (-5 >= n - 6) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
---
+++ js-9/app2.js
@@ -0,0 +1,29 @@
+// if (condition) {
+
+//     // .
+//     // دستورات
+//     // .
+//     // .
+// }
+
+// let num = "a"
+
+// if (num % 2 == 0) {
+//     console.log("even");
+// } else if (num % 2 == 1) {
+//     console.log("odd");
+// } else {
+//     console.log("not number");
+// }
+
+
+let phrase = "green"
+if (phrase == "red") {
+    console.log("stop");
+} else if (phrase == "yellow") {
+    console.log("slow");
+} else if (phrase == "green") {
+    console.log("go");
+} else {
+    console.log("purple");
+}
