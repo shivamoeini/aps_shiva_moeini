c0d443ed63d7c5f78110535f11abf24ae88f2f51
shiva <shivamoeini2000@gmail.com>
2022-02-26 14:59:28 +0330

    cmit62

--- js-25/app3.js
+++ js-25/app3.js
@@ -1,4 +1,4 @@
-//crate c 
+//crate  constructor Person
 function Person(firstName, lastName) {
     this.firstName = firstName;
     this.lastName = lastName;
@@ -7,6 +7,21 @@ Person.prototype.greeting = function() {
     return `welcome tooo ${this.firstName} ${this.lastName}`;
 }
 
-const person1 = new Person('shiva', 'moeini');
+// const person1 = new Person('shiva', 'moeini');
 
-console.log(person1.greeting());
+// console.log(person1.greeting());
+
+// Customer constructor
+function Customer(firstName, lastName, phone, membership) {
+    Person.call(this, firstName, lastName);
+
+    this.phone = phone;
+    this.membership = membership;
+}
+var person = {
+    fullName: function() {
+        return this.firstName + " " + this.lastName;
+    }
+}
+const customer1 = new Customer('Tom', 'Smith', '555-555-5555', 'Standard');
+console.log(customer1);
